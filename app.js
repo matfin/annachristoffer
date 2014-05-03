f2ddcbbcd8048738a64117adeb61019d726a8e3c
Matt Finucane <matfin@gmail.com>
2014-05-04 00:14:42 +0200

    Creating skeleton files
    Views and stylesheets, views, components, cards etc.

---
+++ app.js
@@ -0,0 +1,9 @@
+Meteor.startup = function() {
+	if(Meteor.isClient) {
+		console.log('Client: Meteor starting up.');
+	}
+
+	if(Meteor.isServer) {
+		console.log('Server: Meteor starting up.');
+	}
+};
