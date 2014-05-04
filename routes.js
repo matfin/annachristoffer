b1388be7d354c6a0a22252ab286d90ee3b192f8a
Matt Finucane <matfin@gmail.com>
2014-05-04 19:25:41 +0200

    Routes set up
    Set up routes using iron router for general content, project list
    filters and project detail pages.

--- routes.js
+++ routes.js
@@ -1,9 +1,37 @@
 Router.map(function() {
 
-	// Project list view (all projects)
-	this.route('home', {
-		path: '/',
+	/** 
+	 *	General content views with optional content loaded
+	 *	if the _content parameter is specified, otherwise 
+	 *	load the main list page of unfiltered projects.
+	 */
+	this.route('content', {
+		path: '/content/:_content?',
 		template: 'template_main',
+		data: function() {
+			_content = this.params._content;
+			console.log('Content', _content);
+		},
+		yieldTemplates: {
+			'components_header': {to: 'header'},
+			'views_content': {to: 'content'},
+			'components_footer': {to: 'footer'}
+		}
+	});
+
+	/** 
+	 *	Project list view (all projects) with optional 
+	 *	filter parameter for showing projects only by 
+	 *	their category name.
+	 */ 
+	this.route('list', {
+		path: '/:_category?',
+		template: 'template_main',
+		data: function() {
+			_category = this.params._category;
+
+			console.log('Filter', _category);
+		},
 		yieldTemplates: {
 			'components_header': {to: 'header'},
 			'views_list': {to: 'content'},
@@ -11,13 +39,21 @@ Router.map(function() {
 		}
 	});
 
-	// General content views
-	this.route('about', {
-		path: '/about',
+	/** 
+	 *	Project detail view loaded from project 
+	 *	slug name.
+	 */ 
+	this.route('detail', {
+		path: '/project/:_category/:_project_slug',
 		template: 'template_main',
+		data: function() {
+			_project_slug = this.params._project_slug;
+
+			console.log('Project', _project_slug);
+		},
 		yieldTemplates: {
 			'components_header': {to: 'header'},
-			'views_content': {to: 'content'},
+			'views_detail': {to: 'content'},
 			'components_footer': {to: 'footer'}
 		}
 	});
