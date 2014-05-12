5fa02006fd619a4c9c8a06211790c4fd8a08cd81
Matt Finucane <matfin@gmail.com>
2014-05-12 22:56:57 +0200

    Styling the header

---
+++ client/controllers/views/views_list.js
@@ -0,0 +1,28 @@
+/**
+*	Template - views_list
+*	Callback called automatically when the template instance is created.
+*	@method created
+*	@return undefined
+*/
+Template['views_list'].created = function() {
+};
+
+/**
+*	Template - views_list
+*	Callback called automatically when the template instance is created.
+*	@method rendered
+*	@return undefined
+*/
+Template['views_list'].rendered = function() {
+	$('body').addClass('list');
+};
+
+/**
+*	Template - views_list
+*	Callback called automatically when the template instance is created.
+*	@method destroyed
+*	@return undefined
+*/
+Template['views_list'].destroyed = function() {
+	$('body').removeClass('list');
+};
