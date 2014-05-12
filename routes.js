23cee9883b2029b1ed0a4635239962f58a4d0136
Matt Finucane <matfin@gmail.com>
2014-05-12 20:11:03 +0200

    Adding placeholder German content

--- routes.js
+++ routes.js
@@ -1,36 +1,4 @@
 Router.onRun(function() {
-	/**
-	 *	Populating content
-	 */
-	Api.fetch('content').then(function(data) {
-		_.each(data.items, function(item){
-			App.models.content.insert(item);
-		})
-	}).fail(function(error) {
-		console.log(error);
-	});
-
-	/**
-	 *	Populating projects
-	 */
-	Api.fetch('projects').then(function(data) {
-		_.each(data.items, function(item){
-			App.models.projects.insert(item);
-		})
-	}).fail(function(error) {
-		console.log(error);
-	});
-
-	/**
-	 *	Populating categories
-	 */
-	Api.fetch('categories').then(function(data) {
-		_.each(data.items, function(item){
-			App.models.categories.insert(item);
-		})
-	}).fail(function(error) {
-		console.log(error);
-	});
 });
 
 Router.onBeforeAction(function() {
