63925288d9f5f1c3ae05a5f7a2e8153ad9e7d884
Matt Finucane <matfin@gmail.com>
2015-08-18 13:55:53 +0200

    Removing html5 video player

--- app.js
+++ app.js
@@ -1,21 +1,5 @@
 Meteor.startup(function() {
 	if(Meteor.isClient) {
-
-		/**
-		 *	Setting the device parameters
-		 */
-		// Device.reset();
-
-		/**
-		 *	Set up scrolling for lazy loading
-		 */
-		// Device.initScroll();
-
-		/**
-		 *	Init social plugins
-		 */
-		// Social.Facebook.initSDK();
-		// Social.Pinterest.initSDK();
 	}
 
 	if(Meteor.isServer) {
