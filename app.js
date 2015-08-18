cde5e67d42b8ebcf71e9998bf76faa1f59d85b02
Matt Finucane <matfin@gmail.com>
2015-08-18 11:22:51 +0200

    Added the meteor contentful package. Now fetching content and processing images.

--- app.js
+++ app.js
@@ -16,14 +16,49 @@ Meteor.startup(function() {
 		 */
 		Social.Facebook.initSDK();
 		Social.Pinterest.initSDK();
+		
+		Core = {
+			app: {
+				collections: {
+					entries: new Mongo.Collection('entries'),
+					images: new Mongo.Collection('images'),
+					contentTypes: new Mongo.Collection('contentTypes')
+				}
+			}
+		};
+
+		Meteor.subscribe('entries');
+		Meteor.subscribe('images');
+		Meteor.subscribe('contentTypes');
+
 	}
 
 	if(Meteor.isServer) {
-		console.log('Server: Meteor starting up.');
-		Server.updateAndPublishCollections().then(function(result){
-			console.log('Success updating collections');
-		}).fail(function(error) {
-			console.log('Error updating collections: ', error);
-		});
+		
+		if(Meteor.settings && Meteor.settings.app && Meteor.settings.app.contentful) {
+			console.log('Booting server with Contentful enabled.');
+			MeteorContentful.start().fetch('contentTypes').fetch('entries').fetch('assets');    
+    	ImageProcessor.observe();
+
+    	Meteor.publish('entries', function() {
+    		console.log('Publishing: entries');
+    		return Collections.entries.find({});
+    	});
+
+    	Meteor.publish('images', function() {
+    		console.log('Publishing: images');
+    		return Collections.images.find({});
+    	});
+
+    	Meteor.publish('contentTypes', function() {
+    		console.log('Publishing: contentTypes');
+    		return Collections.contentTypes.find({});
+    	});
+
+		}
+		else {
+			console.log('Booting server with Contentful disabled.');
+		}	
+
 	}
 });
