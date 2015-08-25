3e936eb276cfd17b24cb1a0bb5e75318f3b3d393
Matt Finucane <matfin@gmail.com>
2015-08-25 17:36:24 +0200

    Tidied up tests for the header partial controller.

--- tests/jasmine/client/integration/partials/header.spec.es6.js
+++ tests/jasmine/client/integration/partials/header.spec.es6.js
@@ -42,88 +42,149 @@ describe('partials_header', () => {
 	});
 
 	describe('helpers', () => {
+		describe('categories', () => {
+			it('should call find on the entries collection with the correct parameters when the device is not a mobile device', (done) => {
+				/**
+				 *	Spies
+				 */
+				spyOn(Core.collections.entries, 'find').and.returnValue({});
+				spyOn(Device, 'reset').and.callFake(() => {
+					Device.isMobile = false;
+				});
 
-		it('should call find on the entries collection with the correct parameters', (done) => {
-			/**
-			 *	Spies
-			 */
-			spyOn(Core.collections.entries, 'find').and.returnValue({});
-			/**
-			 *	Run the function and then test
-			 */
-			Template.partials_header.__helpers[' categories']();
-			expect(Core.collections.entries.find).toHaveBeenCalledWith({contentTypeName: 'Project Category'});
+				/** 
+				 *	Reset the device
+				 */
+				Device.reset();
+				/**
+				 *	Run the function and then test
+				 */
+				Template.partials_header.__helpers[' categories']();
+				expect(Core.collections.entries.find).toHaveBeenCalledWith({contentTypeName: 'Project Category'});
 
-			/**
-			 *	Done
-			 */
-			done();
-		});
+				/**
+				 *	Done
+				 */
+				done();
+			});
+	
+			it('should not call find on the entries collection if the device is a mobile device', (done) => {
+				/**
+				 *	Spies
+				 */
+				spyOn(Core.collections.entries, 'find').and.returnValue({});
+				spyOn(Device, 'reset').and.callFake(() => {
+					Device.isMobile = true;
+				});
 
-	});
+				/** 
+				 *	Reset the device
+				 */
+				Device.reset();
+				/**
+				 *	Run the function and then test
+				 */
+				Template.partials_header.__helpers[' categories']();
+				expect(Core.collections.entries.find).not.toHaveBeenCalled();
+				/**
+				 *	Done
+				 */
+				done();
+			});
+		});	
+		
+		describe('active', () => {
+			it('should return the correct classname string if the project category matches that in the router slug', (done) => {
+				/**
+				 *	Spies
+				 */
+				spyOn(Router, 'current').and.returnValue({
+					params: {
+						_slug: 'a-category-slug'
+					}
+				});
 
-	describe('active', () => {
+				/**
+				 *	Dummy data
+				 */
+				let data = {
+					fields: {
+						slug: 'a-category-slug'
+					}
+				};
 
-		it('should return the correct classname string if the project category matches that in the router slug', (done) => {
-			/**
-			 *	Spies
-			 */
-			spyOn(Router, 'current').and.returnValue({
-				params: {
-					_slug: 'a-category-slug'
-				}
+				/**
+				 *	Run the function and the test
+				 */
+				expect(Template.partials_header.__helpers[' active'].call(data)).toEqual('header__navigation__list__item__link--active');
+
+				/**
+				 *	Finished
+				 */
+				done();
 			});
 
-			/**
-			 *	Dummy data
-			 */
-			let data = {
-				fields: {
-					slug: 'a-category-slug'
-				}
-			};
+			it('should return an empty string if the project category does not match that in the router slug', (done) => {
+				/**
+				 *	Spies
+				 */
+				spyOn(Router, 'current').and.returnValue({
+					params: {
+						_slug: 'another-category-slug'
+					}
+				});
 
-			/**
-			 *	Run the function and the test
-			 */
-			expect(Template.partials_header.__helpers[' active'].call(data)).toEqual('header__navigation__list__item__link--active');
+				/**
+				 *	Dummy data
+				 */
+				let data = {
+					fields: {
+						slug: 'a-category-slug'
+					}
+				};
 
-			/**
-			 *	Finished
-			 */
-			done();
+				/**
+				 *	Run the function and the test
+				 */
+				expect(Template.partials_header.__helpers[' active'].call(data)).toEqual('');
+
+				/**
+				 *	Finished
+				 */
+				done();
+			});
 		});
 
-		it('should return an empty string if the project category does not match that in the router slug', (done) => {
-			/**
-			 *	Spies
-			 */
-			spyOn(Router, 'current').and.returnValue({
-				params: {
-					_slug: 'another-category-slug'
-				}
+		describe('top', () => {
+			it('should call depend on the scrolled Dependency', (done) => {
+				/**
+				 *	Spies
+				 */
+				spyOn(Dependencies.scrolled, 'depend').and.returnValue({});
+
+				/**
+				 *	Call the function and then run the tests
+				 */
+				Template.partials_header.__helpers[' top'].call({});
+				expect(Dependencies.scrolled.depend).toHaveBeenCalled();
+
+				/**
+				 *	Done
+				 */
+				done();
 			});
 
-			/**
-			 *	Dummy data
-			 */
-			let data = {
-				fields: {
-					slug: 'a-category-slug'
-				}
-			};
+			it('should return a number', (done) => {
+				/**
+				 *	Call the function and then run the tests
+				 */
+				expect(Template.partials_header.__helpers[' top'].call({})).toEqual(jasmine.any(Number));
+				/**
+				 *	Done
+				 */
+				done();
+			});
 
-			/**
-			 *	Run the function and the test
-			 */
-			expect(Template.partials_header.__helpers[' active'].call(data)).toEqual('');
-
-			/**
-			 *	Finished
-			 */
-			done();
 		});
-
-	});
-
+	});	
 });
